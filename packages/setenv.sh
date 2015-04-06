c3b2d5f83d678c386062d691e339a42bcb7e8c36
Juan Pedro Perez Alcantara <jp.alcantara@geographica.gs>
2015-04-06 16:10:54 +0200

    First commit with Apache Tomcat 8.0.18, Apache Ant 1.9.4, Apache Portable Runtime 1.5.1, and Oracle Server JVM 1.7.0-75.

---
+++ packages/setenv.sh
@@ -0,0 +1,23 @@
+# Custom configuration
+# This is for a little GeoServer on a testing virtual machine.
+# Has the option to use JMX to monitor the JVM.
+
+export LD_LIBRARY_PATH=/usr/local/lib:$CATALINA_HOME/lib:$LD_LIBRARY_PATH
+export JAVA_HOME=/usr/local/jdk1.7.0_75/
+export JRE_HOME=/usr/local/jdk1.7.0_75/jre
+
+MEM=64m
+MMEM=64m
+PMEM=512k
+
+JMX=false
+JMX_PORT=3333
+JMX_HOSTNAME=127.0.0.1
+JMX_ACCESS_FILE=/usr/local/apache-tomcat-8.0.18/conf/jmxremote.access
+JMX_PASSWORD_FILE=/usr/local/apache-tomcat-8.0.18/conf/jmxremote.password
+
+CATALINA_OPTS="-server -d64 -XX:+AggressiveOpts -Djava.library.path=/usr/local/apache-tomcat-8.0.18/lib:/usr/local/lib -Djava.awt.headless=true -XX:MaxGCPauseMillis=500 -Xmx${MEM} -Xms${MEM} -Xincgc"
+
+if $JMX ; then
+    CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.access.file=${JMX_ACCESS_FILE} -Dcom.sun.management.jmxremote.password.file=${JMX_PASSWORD_FILE} -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=${JMX_PORT} -Djava.rmi.server.hostname=${JMX_HOSTNAME}"
+fi
