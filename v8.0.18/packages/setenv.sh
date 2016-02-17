afca72bcd6ced74a14c96a81abf2b5d46dca8124
Juan Pedro Pérez <jp.alcantara@gstgis.com>
2016-02-17 22:58:58 +0100

    Added proxy support.

--- v8.0.18/packages/setenv.sh
+++ v8.0.18/packages/setenv.sh
@@ -5,3 +5,7 @@ CATALINA_OPTS="-server -d64 -XX:+AggressiveOpts -XX:MaxPermSize=${MAXPERMSIZE} -
 if $JMX ; then
     CATALINA_OPTS="${CATALINA_OPTS} -Dcom.sun.management.access.file=${JMX_ACCESS_FILE} -Dcom.sun.management.jmxremote.password.file=${JMX_PASSWORD_FILE} -Dcom.sun.management.jmxremote.authenticate=false -Xdebug -Xrunjdwp:transport=dt_socket,address=62911,server=y,suspend=n -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=${JMX_PORT} -Djava.rmi.server.hostname=${JMX_HOSTNAME} -Dcom.sun.management.jmxremote.rmi.port=${JMX_PORT}"
 fi
+
+if $PROXY ; then
+    CATALINA_OPTS="${CATALINA_OPTS} -Dhttp.proxyHost=${PROXYNAME} -Dhttp.proxyPort=${PROXYPORT}"
+fi
